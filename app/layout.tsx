aae1bd9ace1cad0c88949e3c9da5b17c0e5ac45a
Sayantan <web.dev.sayantan@gmail.com>
2024-06-19 16:51:09 +0530

    <feat> added posthoc for analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,56 +6,59 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/utils/next-auth";
 
 import AuthSessionProvider from "@/context/sessionProvider";
+import { CSPostHogProvider } from "./providers";
 import { cn } from "../lib/utils";
 import dynamic from "next/dynamic";
 
 const ThemeProvider = dynamic(() => import("../components/theme-provider"), {
-  ssr: false,
+	ssr: false,
 });
 
 const inter = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "The pineapple home - Manali",
-  description: "Your second home, in the Himalayas.",
-  metadataBase: new URL("https://thepineapplehome.in"),
+	title: "The pineapple home - Manali",
+	description: "Your second home, in the Himalayas.",
+	metadataBase: new URL("https://thepineapplehome.in"),
 };
 
 const materialSymbols = localFont({
-  variable: "--font-family-symbols", // Variable name (to reference after in CSS/styles)
-  style: "normal",
-  src: "../node_modules/material-symbols/material-symbols-rounded.woff2", // This is a reference to woff2 file from NPM package "material-symbols"
-  display: "block",
-  weight: "100 400 700",
+	variable: "--font-family-symbols", // Variable name (to reference after in CSS/styles)
+	style: "normal",
+	src: "../node_modules/material-symbols/material-symbols-rounded.woff2", // This is a reference to woff2 file from NPM package "material-symbols"
+	display: "block",
+	weight: "100 400 700",
 });
 
 export default async function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  const session = await getServerSession(authOptions);
-  return (
-    <>
-      <html lang="en" className={cn("h-full", materialSymbols.variable)}>
-        <AuthSessionProvider session={session}>
-          <body
-            className={cn(
-              "relative h-full antialiased font-sans",
-              inter.className
-            )}
-          >
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              enableColorScheme
-            >
-              {children}
-            </ThemeProvider>
-          </body>
-        </AuthSessionProvider>
-      </html>
-    </>
-  );
+	const session = await getServerSession(authOptions);
+	return (
+		<>
+			<html lang="en" className={cn("h-full", materialSymbols.variable)}>
+				<CSPostHogProvider>
+					<AuthSessionProvider session={session}>
+						<body
+							className={cn(
+								"relative h-full antialiased font-sans",
+								inter.className,
+							)}
+						>
+							<ThemeProvider
+								attribute="class"
+								defaultTheme="system"
+								enableSystem
+								enableColorScheme
+							>
+								{children}
+							</ThemeProvider>
+						</body>
+					</AuthSessionProvider>
+				</CSPostHogProvider>
+			</html>
+		</>
+	);
 }
